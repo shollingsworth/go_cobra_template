7b5f2b556091ad88ab678d246f09fd53879f8adb
Steven Hollingsworth <749515+shollingsworth@users.noreply.github.com>
2024-02-19 09:52:31 -0800

    Fixed typo

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "changme",
+	Use:   "changeme",
 	Short: "description",
 	CompletionOptions: cobra.CompletionOptions{
 		HiddenDefaultCmd: false,
